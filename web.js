845a39cfea11310b52e65464989fd85a8142ebb5
David Hovanky <dhovanky@gmail.com>
2013-07-09 13:22:53 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var buf = new Buffer(fs.readFileSync("index.html"),'utf-8');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
